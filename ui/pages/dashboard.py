e4fa8cc541ee9a0cbceb4e214be616c094d965ee
anubhavsikka16 <anubhavsikka@gmail.com>
2026-03-28 00:45:33 +0530

    adding app run commands in yaml file

--- ui/pages/dashboard.py
+++ ui/pages/dashboard.py
@@ -3,7 +3,7 @@ class DashboardPage:
         self.page = page
 
     def is_loaded(self):
-        return self.page.locator("[data-test='sidenav']").is_visible()
+        return self.page.locator("[data-test='sidenav']").is_visible() #side panel after login
 
     def click_new_transaction(self):
         self.page.get_by_role("button", name="New").click()
